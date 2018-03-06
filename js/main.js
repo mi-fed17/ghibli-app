2ce1c8a60db62731892baa2af0ef863614148243
Jesper Orb <jesper.orb@zocom.se>
2018-03-06 10:56:41 +0100

    Basic

--- js/main.js
+++ js/main.js
@@ -2,3 +2,22 @@
  * Method chaining *
  *******************/
 
+// const string = "Agneta och Göran har skiljt sig";
+// const splitString = string.split(' ')
+// const reversedString = splitString.reverse()
+// const joinedString = reversedString.join(' ')
+// const subString = joinedString.substr(14);
+// console.log(subString);
+
+fetchMovie();
+
+function fetchMovie(){
+  const url = 'https://ghibliapi.herokuapp.com/films/0440483e-ca0e-4120-8c50-4c8cd9b965d6';
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data){
+      console.log(data);
+    })
+}
