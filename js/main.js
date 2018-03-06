50ff88354c645fdd0b1060f11b94bd8d61ae8878
Jesper Orb <jesper.orb@zocom.se>
2018-03-06 14:40:44 +0100

    Added code to display each character

--- js/main.js
+++ js/main.js
@@ -32,19 +32,30 @@ function fetchMovieThen(){
 
 /* ASYNC FUNCTION */
 
-//! this won't work!!
+// this won't work! `await` cannot be used like this in global scope.
+// the await keyword can only be used inside of an async function.
 //const movie = await fetch();
 
 
+/* 
+ * An async function is created by adding the keyword `async` in front
+ * of the function. We can then use the `await` keyword and make each line
+ * wait for the previous line before continuing.
+*/
 async function fetchTwoMovies(){
   const url = 'https://ghibliapi.herokuapp.com/films/0440483e-ca0e-4120-8c50-4c8cd9b965d6';
+  /* Instead of saying `then()` we `await` the response. The fetch is still async
+   * but the next line will wait the response to return */
   const response = await fetch(url);
+  /* we must still convert the response to JSON, because the response is 
+   * async we must await again, otherwise we will not await the response */
   const movie = await response.json();
+  /* This works with multiple requests in the same function. We can await two different
+   * API calls and the log them at the same time. */
   const response2 = await fetch('https://ghibliapi.herokuapp.com/films/cd3d059c-09f4-4ff3-8d63-bc765a5184fa');
   const movie2 = await response2.json();
   console.log(movie, movie2);
 }
-
 
 async function fetchMovieAsync(){
   const url = 'https://ghibliapi.herokuapp.com/films/0440483e-ca0e-4120-8c50-4c8cd9b965d6';
@@ -58,5 +69,15 @@ async function fetchMovieAsync(){
     promiseArray.push(person);
   }
   const allPeople = await Promise.all(promiseArray);
-  console.log(allPeople);
+  const movieInfo = document.getElementById('movieInfo');
+  let htmlBlock = `<h1>${movie.title} Characters</h1>`;
+  for(let person of allPeople){
+    htmlBlock += `
+      <div class="person">
+        <p><span style="color: #999">name:</span> ${person.name}</p>
+        <p><span style="color: #999">age:</span> ${person.age}</p>
+      </div>
+    `;
+  }
+  movieInfo.innerHTML = htmlBlock;
 }
